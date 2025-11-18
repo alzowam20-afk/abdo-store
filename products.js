761f02cfa8552f174426eacdc1022c43644fa102
alzowam20-afk <alzowam20@gmail.com>
2025-11-18 15:48:53 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,73 @@
+// بيانات المنتجات
+const products = [
+    {
+        id: 1,
+        name: "هاتف ذكي",
+        price: 1200,
+        description: "هاتف ذكي بشاشة 6.5 بوصة وكاميرا متعددة العدسات",
+        image: "images/product1.jpeg"
+    },
+    {
+        id: 2,
+        name: "لابتوب",
+        price: 2500,
+        description: "لابتوب بمعالج قوي وذاكرة عالية للأداء المتميز",
+        image: "images/product2.jpeg"
+    },
+    {
+        id: 3,
+        name: "حمار",
+        price: 320,
+        description: "حمار مطيع وهادئ، مثالي للنقل والاستخدام المنزلي",
+        image: "images/product3.jpeg"
+    },
+    {
+        id: 4,
+        name: "سماعات لاسلكية",
+        price: 150,
+        description: "سماعات عالية الجودة مع تقنية إلغاء الضوضاء",
+        image: "images/product4.jpeg"
+    },
+    {
+        id: 5,
+        name: "ساعة ذكية",
+        price: 400,
+        description: "ساعة ذكية بتقنيات متطورة لتتبع الصحة واللياقة",
+        image: "images/product5.jpeg"
+    },
+    {
+        id: 6,
+        name: "كاميرا رقمية",
+        price: 800,
+        description: "كاميرا احترافية بدقة عالية للتصوير الفوتوغرافي",
+        image: "images/product6.jpeg"
+    },
+    {
+        id: 7,
+        name: "طابعة ليزر",
+        price: 450,
+        description: "طابعة سريعة وجودة عالية للاستخدام المنزلي والمكتبي",
+        image: "images/product7.jpeg"
+    },
+    {
+        id: 8,
+        name: "لوح رسم",
+        price: 180,
+        description: "لوح رسم إلكتروني للمصممين والفنانين",
+        image: "images/product8.jpeg"
+    },
+    {
+        id: 9,
+        name: "شاحن لاسلكي",
+        price: 75,
+        description: "شاحن لاسلكي سريع متوافق مع معظم الأجهزة",
+        image: "images/product9.jpeg"
+    },
+    {
+        id: 10,
+        name: "حقيبة ظهر",
+        price: 90,
+        description: "حقيبة ظهر متينة مع حجرة مخصصة للابتوب",
+        image: "images/product10.jpeg"
+    }
+];
